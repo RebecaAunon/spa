4b35ed0a4d024990031959a70f341657f05aaa83
Rebeca Aunon <raunon@futurespace.es>
2019-11-12 10:27:50 +0100

    buscador

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,17 +9,19 @@ export class HeroeTarjetaComponent implements OnInit {
 
   @Input() heroe: any = {}; //significa que la propiedad heroe puede venir de afuera
   @Input() index: number; //que indice tiene el elemento lo podría haber pasado como parámetro en verHeroe
+  @Output() heroeSeleccionado: EventEmitter<number>; //Le digo que va a emitir un tipo nombre 
 
   constructor( private router: Router ) {
-
+    this.heroeSeleccionado = new EventEmitter(); //inicializamos el evento
    }
 
   ngOnInit() {
   }
 
   verHeroe() {
-    console.log( this.index );
+   // console.log( this.index );
     this.router.navigate(['/heroe', this.index]);
+   // this.heroeSeleccionado.emit( this.index );
   }
 
 }
